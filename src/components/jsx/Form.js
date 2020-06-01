258a9a7754b834217e2f905f30da216352645522
Itritiac <itritiac@gmail.com>
2020-06-01 11:32:24 +0300

    css modules

--- src/components/jsx/Form.js
+++ src/components/jsx/Form.js
@@ -4,15 +4,20 @@ import styles from './../css/form.module.css';
 
 let Form = () => {
 	return(
-		<form>
-		<label>First Name</label><br /><br />
-		<input type="text" id="fname" name="fname" placeholder="FirstName"></input><br /><br />
+		<div className={styles.contactForm}>
+					<div className={styles.formName}>
+						<input type="text" className={styles.name} name="fname" placeholder="Your Name" /><br />
+						<input type="email" className={styles.name} name="fname" placeholder="Your Email" /><br />
+					</div>
+					<div>
+						<input className={styles.formTitle} type="text" id="email" name="email" placeholder="Your Title" />
+					</div>
+					<div>
+						<textarea className={styles.FormText} rows="4" cols="50" name="comment" form="usrform" placeholder="Your Comment"></textarea>
+					</div>
+					<button className={styles.formButton}>Send message</button>
 
-		<label>Second Name</label><br /><br />
-		<input type="text" id="sname" name="sname" placeholder="SecondName"></input><br />
-
-
-		</form>
+				</div>
 
 		);
 }
